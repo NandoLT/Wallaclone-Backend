067b5456d75943fe4227c74edd75ffe7eeea3ad0
AdrianValenzuela <avgvalenzuela@gmail.com>
2021-07-13 22:33:50 +0200

    añadir adverts al initdb

--- libs/initDB/initdb.js
+++ libs/initDB/initdb.js
@@ -1,27 +1,49 @@
-const { mongoose, connectMongoose, User } = require('../../models');
+const { mongoose, connectMongoose, User, Advert } = require('../../models');
 
-const fs = require('fs')
-const data = fs.readFileSync((__dirname + '/users.json'), 'utf8')
-const dataParse = JSON.parse(data)
+const fs = require('fs');
+const userData = fs.readFileSync((__dirname + '/users.json'), 'utf8');
+const userDataParse = JSON.parse(userData);
+const advertData = fs.readFileSync((__dirname + '/adverts.json'), 'utf8');
+const advertDataParse = JSON.parse(advertData);
 
 async function  dropChargeBd(){
-    if(User){
+    if(User) {
         try {
-            await User.deleteMany()
-            console.log('Collection deleted')
-            await User.insertMany(dataParse)
-            console.log("Data inserted")
-            console.log("Closing DB Conecction...")
-            mongoose.connection.close()
+            await User.deleteMany();
+            console.log('Collection deleted');
+            await User.insertMany(userDataParse);
+            console.log("Data inserted");
+            console.log("Closing DB Conecction...");
+            // mongoose.connection.close();
         } catch (err) {
-            console.log(err)
+            console.log(err);
         }
     } else {
         try {
-            await User.insertMany(dataParse)
-            console.log("Data inserted")
+            await User.insertMany(userDataParse);
+            console.log("Data inserted");
         } catch(err) {
-            console.log(err)
+            console.log(err);
+        }
+    }
+
+    if(Advert) {
+        try {
+            await Advert.deleteMany();
+            console.log('Collection deleted');
+            await Advert.insertMany(advertDataParse);
+            console.log("Data inserted");
+            console.log("Closing DB Conecction...");
+            // mongoose.connection.close();
+        } catch (err) {
+            console.log(err);
+        }
+    } else {
+        try {
+            await User.insertMany(advertDataParse);
+            console.log("Data inserted");
+        } catch(err) {
+            console.log(err);
         }
     }
 }

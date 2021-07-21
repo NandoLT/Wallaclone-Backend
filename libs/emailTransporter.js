8b552d20c0a834fbf57d8ab5e6eae9a0dbc99c32
Fernando López Trejo <fernando_lt@hotmail.es>
2021-07-21 10:55:38 +0200

    Arreglamos seguridad smtp en la rama

--- libs/emailTransporter.js
+++ libs/emailTransporter.js
@@ -3,14 +3,14 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-      user: 'akkerstudio@gmail.com',
-      pass: 'suksqelqmrpjgtbe', 
-    },
-  });
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: process.env.EMAIL_SECURE,
+  auth: {
+    user: process.env.EMAIL_AUTH_USER,
+    pass: process.env.EMAIL_AUTH_PASS, 
+  },
+});
 
 transporter.verify().then(() => {
     console.log('Ready for send emails');

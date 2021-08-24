8abd4697c0488c3b1e253d04a519daf5428925ea
Fernando López Trejo <fernando_lt@hotmail.es>
2021-08-24 23:45:13 +0200

    Add Chats model

--- models/Chats.js
+++ models/Chats.js
@@ -3,9 +3,23 @@
 // libraries requires
 const mongoose = require('mongoose');
 
-const chatsSchema = mongoose.Schema ({
-
-});
+const chatsSchema = mongoose.Schema (
+    {
+        conversationId: {
+            type: String
+        },
+        members: {
+            type: Array
+        },
+        sender: {
+            type: String
+        },
+        text: {
+            type: String
+        }
+    },
+    { timestamps: true }
+);
 
 const Chats = mongoose.model('Chats', chatsSchema);
 
